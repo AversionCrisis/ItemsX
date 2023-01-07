5308ad1216433cf5bd3162e1c3874ab97b7cca6d
AversionCrisis <itscinnahome@gmail.com>
2023-01-07 02:14:24 -0500

    Initial commit

---
+++ src/main/java/net/twilight/itemsx/ItemsXClient.java
@@ -0,0 +1,11 @@
+package net.twilight.itemsx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemsXClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
